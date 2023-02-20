423fc77bd3bf2855a690203e8c4923e5e0e2f2c1
Shen Tianqi <1055862487@qq.com>
2023-02-20 16:10:49 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import cn.edu.nju.stq.A;
+
+public class Main {
+    A a;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/cn/edu/nju/stq/A.java
@@ -0,0 +1,4 @@
+package cn.edu.nju.stq;
+
+public class A {
+}
