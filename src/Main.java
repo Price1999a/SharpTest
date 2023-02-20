5ab5a7eb3b143cf4eb179e15212b8ac2210fe3ca
Shen Tianqi <1055862487@qq.com>
2023-02-20 16:23:15 +0800

    inc2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import cn.edu.nju.stq.B;
 
 public class Main {
     A a = new B();
+    A a2 = new A();
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Main main = new Main();
+        main.a.pubf1();
     }
 }
